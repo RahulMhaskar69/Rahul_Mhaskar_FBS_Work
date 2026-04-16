686470c5b3fea0cbb95c18c46ff3b2f3051ec866
RahulMhaskar69 <rahulmhaskar239@gmail.com>
2026-04-16 20:31:51 +0530

    Delete Assignment 0 directory

--- Assignment 0/AS02.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{ float radius = 6.5 ,area;
-    const float PI = 3.14;
-    area = PI * radius * radius ;
-    printf (" Area of Circle : %.2f" , area);
-    return 0; 
-}
--- Assignment 0/AS07.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    int total_minutes = 135;
-    int hours = total_minutes / 60;
-    int mins = total_minutes % 60;
-    printf("%d minutes = %d hours and %d minutes\n", total_minutes, hours, mins);
-    return 0;
-}
